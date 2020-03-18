4d63dea8aed568ea36862e5b381d17973325bf13
Marvin Buss <marvin.buss@gmail.com>
2020-03-18 21:52:51 +0100

    updated code strcuture

--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,6 @@
 import sys
 
+
 def required_parameters_provided(parameters, keys, message="Required parameter not found in your parameters file. Please provide a value for the following key: "):
     for key in keys:
         if not parameters.get(key, None):
